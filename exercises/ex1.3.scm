f03ad63dedd5ccf45cd148172ebb84127348d2aa
zrottman <zrottman@example.com>
2023-05-23 15:43:36 -0700

    Complete ex1.3

---
+++ exercises/ex1.3.scm
@@ -0,0 +1,6 @@
+(define (square x) (* x x))
+(define (sum_squares a b) (+ (square a) (square b)))
+(define (f a b c)
+    (cond ((and (< a b) (< a c)) (sum_squares b c))
+          ((and (< b a) (< b c)) (sum_squares a c))
+          (else (sum_squares a b))))
